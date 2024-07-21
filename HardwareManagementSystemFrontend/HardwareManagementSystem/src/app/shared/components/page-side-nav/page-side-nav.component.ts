407dddf99a70270213b82aef518365e883ca9899
Rajnish Singh <Rajnish.Singh@kpit.com>
2024-07-21 13:18:45 +0530

    Auth module added, hardware module added

--- HardwareManagementSystemFrontend/HardwareManagementSystem/src/app/shared/components/page-side-nav/page-side-nav.component.ts
+++ HardwareManagementSystemFrontend/HardwareManagementSystem/src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+export interface NavigationItem {
+  value: string;
+  link: string;
+}
 @Component({
   selector: 'page-side-nav',
   templateUrl: './page-side-nav.component.html',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class PageSideNavComponent {
   panelName: string = 'Admin Panel';
+  navItems: NavigationItem[] = [];
+
+  constructor(){
+    this.navItems=[
+      { value: 'Home', link: 'home' },
+      { value: 'Request', link: 'Request' },
+      { value: 'Issue/Return', link: 'Issue-Return' },
+      { value: 'Search', link: 'Search' },
+      { value: 'HW Management', link: 'hardware/management' },
+      { value: 'User Management', link: 'User-Management' },
+      { value: 'About', link: 'About' },
+    ]
+  }
 }
